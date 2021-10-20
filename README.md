deb6e5ded94c8b359264e3d92b5e1c224b34dfd7
Ivan Diaz Garnacho <diazgari@emea.roche.com>
2021-10-21 00:04:42 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# terraform-webapp-test
+
+Testing purposes. Building the whole web application architecture in AWS using Terraform.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a test webapp</h1>
+      <h2>
+        Visit <a href="https://wannalearncloud.com">wannalearncloud.com</a> for
+        more amazing posts!
+      </h2>
+    </div>
+  );
+}
+
+export default App;
